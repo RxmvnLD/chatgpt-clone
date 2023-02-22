9836ff132271104af3e6b5f4182869543a2c02de
Roman Lemus <roman09071999@gmail.com>
2023-02-22 13:24:20 -0600

    -

--- src/pages/api/send-prompt.ts
+++ src/pages/api/send-prompt.ts
@@ -35,8 +35,6 @@ export default async function handler(
       res.status(200).json(result.data);
     }
     console.log(result);
-
-    // res.status(200).json({ message: "success" });
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error: error });
   }
