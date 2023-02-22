95d91f5390bc118e09fbbdac0c0b5e5ae8a0a9bd
Roman Lemus <roman09071999@gmail.com>
2023-02-22 13:24:08 -0600

    created chat api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import Aside from "@/components/Aside";
 import Conversation from "@/components/Conversation";
 
+import axios from "axios";
+import { useEffect } from "react";
+
 function Layout({ children }: { children: React.ReactNode }) {
+  //fetching
+  const sendPrompt = async () => {
+    try {
+      const result = await axios.post("/api/chat", {
+        userPrompt: "Hello, how are you?"
+      });
+      console.log("res", result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    sendPrompt();
+    console.log(process.env.OPENIA_API_KEY);
+  }, []);
+
   return (
     <div className="relative flex flex-row overflow-hidden">{children}</div>
   );
